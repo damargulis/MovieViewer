fd7d7c455c73fa13018bde4c8e7d3e88f1b28f99
damargulis <damargulis@wustl.edu>
2016-01-28 16:33:04 -0600

    Changed tableView to collectionView

---
+++ MovieViewer/CollectionMovieCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionMovieCell.swift
+//  MovieViewer
+//
+//  Created by Daniel Margulis on 1/28/16.
+//  Copyright © 2016 Daniel Margulis. All rights reserved.
+//
+
+import UIKit
+
+class CollectionMovieCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- MovieViewer/MovieViewController.swift
+++ MovieViewer/MovieViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 import AFNetworking
 import MBProgressHUD
  
-class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    @IBOutlet weak var tableView: UITableView!
+class MovieViewController: UIViewController, UICollectionViewDataSource{
     
+    @IBOutlet weak var collectionView: UICollectionView!
     var movies: [NSDictionary]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
+        collectionView.dataSource = self
         
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
-        tableView.insertSubview(refreshControl, atIndex: 0)
+        collectionView.insertSubview(refreshControl, atIndex: 0)
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -49,7 +47,7 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
                             print("response: \(responseDictionary)")
                             
                             self.movies = responseDictionary["results"] as! [NSDictionary]
-                            self.tableView.reloadData()
+                            self.collectionView.reloadData()
                             MBProgressHUD.hideHUDForView(self.view, animated: true)
                             
                     }
@@ -75,21 +73,25 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
 
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else{
+            return 0
+        }
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("collectionMovieCellid", forIndexPath: indexPath) as! CollectionMovieCell
         
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         let posterPath = movie["poster_path"] as! String
-        
         let imageUrl = NSURL(string: baseUrl + posterPath)
         
         cell.posterView.setImageWithURL(imageUrl!)
-        cell.titleLabel.text = title
-        cell.overviewLabel.text = overview
-        
         print("row \(indexPath.row)")
         return cell
         
@@ -121,12 +123,12 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
                             print("response: \(responseDictionary)")
                             
                             self.movies = responseDictionary["results"] as! [NSDictionary]
-                            self.tableView.reloadData()
+                            self.collectionView.reloadData()
                             MBProgressHUD.hideHUDForView(self.view, animated: true)
                             
                     }
                 }
-                self.tableView.reloadData()
+                self.collectionView.reloadData()
                 
                 refreshControl.endRefreshing()
 
